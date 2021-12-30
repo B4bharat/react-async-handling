8ba423dd89d5e48cf41cb945b210a2a0c4b41bcd
Bharat Poptwani <bharat.p@goodworker.in>
2021-12-30 11:44:36 +0530

    basic movie fetching logic using React

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movie) => {
+      return {
+        title: movie.title,
+        id: movie.episode_id,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      };
+    });
+
+    setMovies(transformedMovies);
+    setIsLoading(false);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && movies.length === 0 && <p>No movies to show</p>}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
